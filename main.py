b9306f78520e45da5b3702ea0b60e3da2f25655b
Celdor <cdemoulin@protonmail.com>
2022-09-02 21:57:41 +0200

    bissl an main.py gebastelt

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
-import math
+from selenium.webdriver.chrome.options import Options
 
+
+
+
+
+options = webdriver.ChromeOptions() 
+options.add_argument("user-data-dir=/home/cederic/.mozilla/firefox/e5vg9le0.default")
 
 driver = webdriver.Firefox()
-
 driver.get('https://web.whatsapp.com')
 
 print(f'{driver.title}')
