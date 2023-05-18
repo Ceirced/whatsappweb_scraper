cb3a7a3f7d2e36af927dca607b933e61a0e1c3fc
Cederic Demoulin <cdemoulin@protonmail.com>
2023-05-18 19:41:56 +0200

    added command line option to add a single person

--- add_user.py
+++ add_user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 import json
-
+import argparse 
 
 def add_user(name: str):
     """adds a user to the users.json file"""
@@ -16,14 +16,26 @@ def add_user(name: str):
         json.dump(users, f, indent=4)
     print(f'added user {name} to users.json')
 
+def userCount():
+    """returns the number of users in users.json"""
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    return len(users["users"])
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='add a user to the users.json file')
+    parser.add_argument('-n' , '--name', type=str,help='name of user to add')
+    args = parser.parse_args()
+
+    if args.name:
+        add_user(args.name)
+        print(f'number of users: {userCount()}')
+        quit()
+
     add = True
     while add:
         name = input('name of user to add: ')
         add_user(name)
-        with open('users.json', 'r') as f:
-            users = json.load(f)
-    
-        print(f'number of users: {len(users["users"])}')
+        print(f'number of users: {userCount()}')
         add = input('add another user? (y/n) ') == 'y'
 
