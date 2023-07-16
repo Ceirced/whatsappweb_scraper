147259afdd1f99b322464542db33be998addaad2
Cederic Demoulin <cdemoulin@protonmail.com>
2023-07-16 17:51:21 +0200

    added start script for server

---
+++ whats_feed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source $HOME/whatsappweb_scraper/env/bin/activate
+
+flask --app $HOME/whatsappweb_scraper/app/app run --port=5040 &
+
+SERVER_PID=$!
+
+
+
+firefox -new-tab http://localhost:5040
+
+echo Server PID: $SERVER_PID
+
+read -p "Press any key to stop... " -r
+
+
+kill $SERVER_PID
+
