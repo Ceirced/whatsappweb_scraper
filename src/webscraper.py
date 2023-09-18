73bb7c94c88c0639dcd9ae2d88044f2fdfcbd2c7
Cederic Demoulin <cdemoulin@protonmail.com>
2023-09-18 23:10:24 +0200

    added user agent, worked

--- src/webscraper.py
+++ src/webscraper.py
@@ -5,13 +5,17 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
+USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+print("test")
+
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
+chrome_options.add_argument(f'--user-agent={USERAGENT}')
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 
-driver.get('https://www.google.com/')
+driver.get('https://open.spotify.com/artist/1LDYdZ0TAovDFvPtQLm6e9')
 print('title: %s' % driver.title)
 driver.save_screenshot('/app/data/screenie3.png')
 driver.quit()
