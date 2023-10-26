85ce5dec86dbd6cae736f85f9b66eccf52a7871d
Cederic Demoulin <cdemoulin@protonmail.com>
2023-10-26 22:43:56 +0200

    docker start

---
+++ mariadb/database_setup.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS `whatsfeed`;
+CREATE USER IF NOT EXISTS 'cederic' @'localhost' IDENTIFIED BY 'complicated';
+GRANT ALL PRIVILEGES ON whatsfeed.* TO 'cederic' @'localhost';
+FLUSH PRIVILEGES;
+USE `whatsfeed`;
+CREATE TABLE IF NOT EXISTS `users` (
+    `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `contact_name` date NOT NULL
+);
+CREATE TABLE IF NOT EXISTS `pictures` (
+    `image_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `picture_filename` varchar(255) NOT NULL,
+    `timestamp` BIGINT NOT NULL,
+    `user_id` int(11) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+CREATE TABLE IF NOT EXISTS `status` (
+    `status_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `status` varchar(255) NOT NULL,
+    `timestamp` BIGINT NOT NULL,
+    `user_id` int(11) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
