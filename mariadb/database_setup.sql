7a756d3068f7b8afbfe7697a061559140b7e9681
Cederic Demoulin <cdemoulin@protonmail.com>
2023-11-01 21:40:39 +0100

    made unique constraint for columns and specified a subnet

--- mariadb/database_setup.sql
+++ mariadb/database_setup.sql
@@ -1,17 +1,18 @@
 CREATE DATABASE IF NOT EXISTS `whatsfeed`;
-CREATE USER IF NOT EXISTS 'cederic' @'localhost' IDENTIFIED BY 'complicated';
-GRANT ALL PRIVILEGES ON whatsfeed.* TO 'cederic' @'localhost';
+CREATE USER IF NOT EXISTS 'cederic'@'172.24.0.1' IDENTIFIED BY 'complicated';
+GRANT ALL PRIVILEGES ON whatsfeed.* TO 'cederic'@'172.24.0.1';
 FLUSH PRIVILEGES;
 USE `whatsfeed`;
 CREATE TABLE IF NOT EXISTS `users` (
     `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `contact_name` varchar(255) NOT NULL
+    `contact_name` varchar(255) NOT NULL UNIQUE
 );
 CREATE TABLE IF NOT EXISTS `pictures` (
     `image_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `picture_filename` varchar(255) NOT NULL,
     `timestamp` BIGINT NOT NULL,
     `user_id` int(11) NOT NULL,
+    UNIQUE (`picture_filename`, `timestamp`, `user_id`),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 CREATE TABLE IF NOT EXISTS `status` (
@@ -19,5 +20,6 @@ CREATE TABLE IF NOT EXISTS `status` (
     `status` varchar(255) NOT NULL,
     `timestamp` BIGINT NOT NULL,
     `user_id` int(11) NOT NULL,
+    UNIQUE (`status`, `timestamp`, `user_id`),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
