db592df0209ffa9e4a98532c40c81036b19119d9
Cederic Demoulin <cdemoulin@protonmail.com>
2023-11-17 21:35:02 +0100

    Add user status endpoints and update models.py

--- flask-tdd-docker/app/api/users.py
+++ flask-tdd-docker/app/api/users.py
@@ -1,6 +1,6 @@
 from flask import jsonify
 from app.api import bp
-from app.models import users
+from app.models import users, status
 from app import db
 from sqlalchemy import select
 
@@ -20,7 +20,7 @@ def get_users():
             }
         })
 
-@bp.route('/user/<int:user_id>', methods=['GET'])
+@bp.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     id, contact_name = db.session.execute(select(users.user_id,users.contact_name).where(users.user_id==user_id)).first()
     if not id and contact_name:
@@ -34,5 +34,43 @@ def get_user(user_id):
             'data': {
                 'user_id': id,
                 'contact_name': contact_name
+            }
+        })
+
+# get all status of a user
+@bp.route('/users/<int:user_id>/status', methods=['GET'])
+def get_user_status(user_id):
+    status_list = db.session.query(status).filter(status.user_id==user_id).all()
+    data = [status.to_dict() for status in status_list]
+    if not status_list:
+        return jsonify({
+            'status': 'fail',
+            'message': 'User does not exist'
+        })
+    else:
+        return jsonify({
+            'status': 'success',
+            'data': {
+                'user_id': user_id,
+                'contact_name': status_list[0].user.contact_name,   
+                'status': data
+            }
+        })
+
+# get all status
+@bp.route('/status', methods=['GET'])
+def get_all_status():
+    status_list = db.session.query(status).all()
+    data = [status.to_dict() for status in status_list]
+    if not status_list:
+        return jsonify({
+            'status': 'fail',
+            'message': 'No status in database'
+        })
+    else:
+        return jsonify({
+            'status': 'success',
+            'data': {
+                'status': data
             }
         })
