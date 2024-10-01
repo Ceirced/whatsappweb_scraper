ef44dd78e9ca2981b70b4e4f24367ddfac72a51c
Cederic Demoulin <cdemoulin@protonmail.com>
2024-10-01 12:25:25 +0200

    removed unnecessary imports

--- flask-tdd-docker/app/main/routes.py
+++ flask-tdd-docker/app/main/routes.py
@@ -1,21 +1,27 @@
-from flask import jsonify, render_template, request, url_for, redirect, flash
+from flask import render_template
 from app.main import bp
-from app.models import users, status, get_feed, format_timestamp, pictures
-from app import db
-from sqlalchemy import select
-from datetime import datetime
+from app.models import get_feed, format_timestamp
 
 
-@bp.route('/')
-@bp.route('/index')
+@bp.route("/")
+@bp.route("/index")
 def index():
     feed_data = get_feed()
-    human_timestamp = {post.timestamp: format_timestamp(post.timestamp) for post in feed_data}
-    return render_template('feed.html', feed_data=feed_data, human_timestamp=human_timestamp)
+    human_timestamp = {
+        post.timestamp: format_timestamp(post.timestamp) for post in feed_data
+    }
+    return render_template(
+        "feed.html", feed_data=feed_data, human_timestamp=human_timestamp
+    )
 
-@bp.route('/profile/<string:username>')
+
+@bp.route("/profile/<string:username>")
 def profile(username):
     feed_data = get_feed(username)
-    
-    human_timestamp = {post.timestamp: format_timestamp(post.timestamp) for post in feed_data}
-    return render_template('profile.html', feed_data=feed_data, human_timestamp=human_timestamp)
+
+    human_timestamp = {
+        post.timestamp: format_timestamp(post.timestamp) for post in feed_data
+    }
+    return render_template(
+        "profile.html", feed_data=feed_data, human_timestamp=human_timestamp
+    )
