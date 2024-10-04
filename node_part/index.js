b29dc5c8043398ee2d9e597809a016455524c768
Cederic Demoulin <cdemoulin@protonmail.com>
2024-10-04 11:31:30 +0200

    added loading screen and catching errors

--- node_part/index.js
+++ node_part/index.js
@@ -18,10 +18,24 @@ whatsapp.on("qr", (qr) => {
 });
 
 
-// Log when the client is ready
-whatsapp.on("ready", async () => {
-  console.log("Client is ready!");
+whatsapp.on('loading_screen', (percent, message) => {
+  console.log('LOADING SCREEN', percent, message);
+});
+
+whatsapp.on('ready', async () => {
+  console.log('READY');
+  const debugWWebVersion = await whatsapp.getWWebVersion();
+  console.log(`WWebVersion = ${debugWWebVersion}`);
+
   await processContacts();
+
+  whatsapp.pupPage.on('pageerror', function (err) {
+    console.log('Page error: ' + err.toString());
+  });
+  whatsapp.pupPage.on('error', function (err) {
+    console.log('Page error: ' + err.toString());
+  });
+
 });
 
 // Process contacts and check for new profile pictures
@@ -45,11 +59,15 @@ async function processContacts() {
 async function checkProfilePicture(contact) {
   try {
     const url = await whatsapp.getProfilePicUrl(contact.id._serialized);
+    if (!url) {
+      console.log(`No profile picture found for ${contact.name}`);
+      return;
+    }
     const picture_id = pictureUrlToId(url);
     const isNew = await checkIfPictureNew(picture_id);
 
     if (isNew) {
-      console.log("New profile picture detected!");
+      console.log(`New profile picture for ${contact.name} detected!`);
     } else {
       console.log(`Profile picture not changed for ${contact.name}`);
     }
